22b57cac122aeda7d85ed9e90d179e855b9c8ea9
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2019-10-22 11:34:30 +1100

    minor changes

--- CustomMouseFunctions/custom_mouse_functions.py
+++ CustomMouseFunctions/custom_mouse_functions.py
@@ -8,23 +8,44 @@ from scipy import ndimage as ndi
 import numpy as np
 import napari
 from qtpy import QtWidgets, QtGui, QtCore
+from skimage.measure import regionprops_table
+import pandas as pd
+from pandas_qt_table import pandasModel
+from qtpy.QtWidgets import QApplication, QTableView
 
 
+def create_regionprop_text(df, index):
+    if index is None: # In viewer, but not over a pixel
+        oblabel = "outside"
+    if index == 0:
+        oblabel = "background"
+    else:
+        oblabel = str(index)
+    subset = df[df["label"]==index]
+    text = f"<h1> Object ID: {oblabel}<h1><p>"
+    for col in df.columns:
+        text += f"{col}: {str(subset[col].values)}<br>"
+    return(text)
 
 with napari.gui_qt():
     np.random.seed(1)
     viewer = napari.Viewer()
     blobs = data.binary_blobs(length=128, volume_fraction=0.1, n_dim=2)
     labeled = ndi.label(blobs)[0]
+
+    # Measure some properties of the blobs and create a DataFrame
+    measurements = regionprops_table(label_image= labeled, properties=('label','area', 'bbox'))
+    df = pd.DataFrame(measurements)
+    # Create a Qt TableView from the pandas table
+    model = pandasModel(df[df.label==1]) # dirty hack so we just have a one-row table
+    view = QTableView()
+    view.setModel(model)
+    view.resize(800, 200)
+    view.show()
+    text = QtWidgets.QLabel("asdsad")
+    text.setText("<b>asdsad</b>: asdsad\n <h1> asdsad</h1>")
+    text.show()
     labels_layer = viewer.add_labels(labeled, name='blob ID')
-    #def showdialog():
-    d = QtWidgets.QDialog()
-    textlabel = QtWidgets.QLabel("234234",d)
-    b1 = QtWidgets.QPushButton("ok",d)
-    b1.move(50,50)
-    d.setWindowTitle("Dialog")
-    #d.setWindowModality(Qt.ApplicationModal)
-    d.show()
 
     @viewer.mouse_drag_callbacks.append
     def get_ndisplay(viewer, event):
@@ -33,8 +54,12 @@ with napari.gui_qt():
 
     @labels_layer.mouse_move_callbacks.append
     def move(layer, event):
-        print(event.pos)
-        textlabel.setText(str(layer.get_value()))
+        #print(event.pos)
+        label_nr = layer.get_value()
+        subset = df[df["label"]== label_nr]
+        model.updateData(subset)
+        text.setText(create_regionprop_text(df, label_nr))
+
 
     @labels_layer.mouse_drag_callbacks.append
     def get_connected_component_shape(layer, event):
